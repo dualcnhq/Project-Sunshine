7187fca6c658d721fe354549b6300d58b33a196e
dualcnhq <nhq1829@gmail.com>
2015-02-21 09:44:16 +0800

    Added dummy data

--- app/src/main/java/com/dualcnhq/projectsunshine/MainActivity.java
+++ app/src/main/java/com/dualcnhq/projectsunshine/MainActivity.java
@@ -11,6 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -59,6 +63,20 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
+            // Create some dummy data for the ListView.
+            String[] data = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+            
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
