7d153df6adfbac355c2e329b14e8f4445cb538ab
dualcnhq <nhq1829@gmail.com>
2015-02-21 10:01:55 +0800

    Set listview adapter

--- app/src/main/java/com/dualcnhq/projectsunshine/MainActivity.java
+++ app/src/main/java/com/dualcnhq/projectsunshine/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -90,6 +91,11 @@ public class MainActivity extends ActionBarActivity {
                     weekForecast); // The arrayList name
             
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // Get a reference to the ListView, and attach this adapter to it.
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+            
             return rootView;
         }
     }
